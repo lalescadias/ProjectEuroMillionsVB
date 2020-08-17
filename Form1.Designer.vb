cb31cb10c9f1b6b75d66866e7ed4db480b5941c8
Lalesca Dias <lalescadias6@gmail.com>
2020-08-17 16:18:37 +0100

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,267 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.MenuText
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Red
+        Me.Button1.Location = New System.Drawing.Point(12, 427)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(153, 32)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Gerar chave"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.WindowsEuroMihloes.My.Resources.Resources.euro
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(210, 39)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(167, 431)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(13, 145)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(188, 25)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Numeros gerados:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label2.Location = New System.Drawing.Point(12, 253)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(180, 25)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Estrelas geradas:"
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox8.Image = CType(resources.GetObject("PictureBox8.Image"), System.Drawing.Image)
+        Me.PictureBox8.Location = New System.Drawing.Point(1, 0)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox8.TabIndex = 33
+        Me.PictureBox8.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 17.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.ForestGreen
+        Me.Label3.Location = New System.Drawing.Point(89, 6)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(207, 29)
+        Me.Label3.TabIndex = 34
+        Me.Label3.Text = "EURO MILHÕES"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = Global.WindowsEuroMihloes.My.Resources.Resources.euro
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(380, 39)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(167, 431)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 35
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackgroundImage = Global.WindowsEuroMihloes.My.Resources.Resources.euro
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(550, 39)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(167, 431)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox3.TabIndex = 36
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackgroundImage = Global.WindowsEuroMihloes.My.Resources.Resources.euro
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(720, 39)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(167, 431)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox4.TabIndex = 37
+        Me.PictureBox4.TabStop = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(15, 173)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(169, 69)
+        Me.ListBox1.TabIndex = 38
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(17, 281)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(120, 69)
+        Me.ListBox2.TabIndex = 39
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackgroundImage = Global.WindowsEuroMihloes.My.Resources.Resources.euro
+        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
+        Me.PictureBox5.Location = New System.Drawing.Point(890, 39)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(167, 431)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox5.TabIndex = 40
+        Me.PictureBox5.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.MenuText
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Red
+        Me.Button2.Location = New System.Drawing.Point(12, 389)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(153, 32)
+        Me.Button2.TabIndex = 41
+        Me.Button2.Text = "LIMPAR"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label4.Location = New System.Drawing.Point(10, 87)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(174, 25)
+        Me.Label4.TabIndex = 42
+        Me.Label4.Text = "Quantas chaves:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"1", "2", "3", "4", "5"})
+        Me.ComboBox1.Location = New System.Drawing.Point(16, 115)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 44
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(1064, 482)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Euro Milhões"
+        Me.TransparencyKey = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,151 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim pic() As PictureBox 'create a variable that stores a picturebox array
+        pic = New PictureBox() {PictureBox1, PictureBox2, PictureBox3, PictureBox4, PictureBox5}
+        pic(0) = PictureBox1
+        pic(1) = PictureBox2
+        pic(2) = PictureBox3
+        pic(3) = PictureBox4
+        pic(4) = PictureBox5
+        Dim randomNumbers(5) As Integer 'array of random numbers
+        Dim randomStars(1) As Integer 'array of random stars
+        Dim value As String
+        Dim i As Integer
+        Dim num As Integer = Val(ComboBox1.Text) 'the variable "num" receives the value of the combobox
+        If num > 5 Or num <= 0 Then 'checks if the value entered in the combobox is greater than 5 or less than 0
+            MsgBox("INSIRA UM VALOR VALIDO", MsgBoxStyle.Critical) ' send error message 
+
+        ElseIf ListBox1.Items.Count > 0 Then 'check if there is anything in the listbox if it exists it asks to clean
+            MsgBox("LIMPE A TELA ANTES DE GERAR UM NOVO CODIGO", MsgBoxStyle.Information) ' send info message 
+        Else
+            For i = 0 To num - 1 'after checking, loop from 0 to "num"
+                Dim orderedKey As String = ""
+                Dim orderedKey2 As String = ""
+                Dim intArray = GenerateRandomNumber(randomNumbers, 1, 51) 'calls the function passing the parameters of the array size, initial and final value
+                Dim stars = GenerateRandomNumber(randomStars, 1, 13) 'calls the function passing the parameters of the array size, initial and final value
+                For Each value In intArray
+                    orderedKey = orderedKey & " - " & value
+                Next
+                orderedKey = orderedKey.Substring(3) 'subtracts the amount of strings passed in the substring parameter
+
+                For Each value In stars
+                    orderedKey2 = orderedKey2 & " - " & value
+                Next
+                orderedKey2 = orderedKey2.Substring(3) 'subtracts the amount of strings passed in the substring parameter
+                ListBox1.Items.Insert((i), orderedKey) 'inserts from the "i" the "orderedkey" value in the listbox
+                ListBox2.Items.Insert((i), orderedKey2) 'inserts from the "i" the "orderedkey2" value in the listbox
+
+                DrawNumbers(intArray, pic(i)) 'calls the function passing the numbers already ordered and in which picturebox you want to draw
+                Drawstars(stars, pic(i)) 'calls the function passing the stars already ordered and in which picturebox you want to draw
+            Next
+        End If
+
+    End Sub
+    Public Sub DrawNumbers(ByVal drawnKeys() As Integer, ByVal imageCard As PictureBox)
+        Dim myPen As Pen 'creates a variable with a pen attribute
+        myPen = New Pen(Drawing.Color.Black, 3) 'in the variable put the color you want and the thickness of the stroke
+        Dim myGraphics As Graphics = imageCard.CreateGraphics 'create a variable that becomes the object to be drawn, that object and a picturebox received in the function
+        Dim i As Integer
+        Dim x As Integer = -22 'value that starts point x to make the circle, left or rigth
+        Dim y As Integer = 10 'value that starts point y to make the circle, up or down
+        Dim z As Integer = 28 'distance the circle is from the other
+        For i = 0 To drawnKeys.Length - 1 'cycle that runs through the array size -1
+            Select Case drawnKeys(i)'checks the value one by one, and drawing in the respective coordinate specified in each case
+                Case < 7 'Case 1, 2, 3, 4, 5, 6
+                    myGraphics.DrawEllipse(myPen, x + (z * drawnKeys(i)), y, 15, 15)
+                                           'the pen, x coordinate, y coordinate, length and height to make the perfect circle
+                Case 7 To 12 'Case 7, 8, 9, 10, 11, 12
+                    myGraphics.DrawEllipse(myPen, x + (z * (drawnKeys(i) - 6)), y + (z * 1), 15, 15)
+                Case 13 To 18
+                    myGraphics.DrawEllipse(myPen, x + (z * (drawnKeys(i) - 12)), y + (z * 2), 15, 15)
+                Case 19 To 24
+                    myGraphics.DrawEllipse(myPen, x + (z * (drawnKeys(i) - 18)), y + (z * 3), 15, 15)
+                Case 25 To 30
+                    myGraphics.DrawEllipse(myPen, x + (z * (drawnKeys(i) - 24)), y + (z * 4), 15, 15)
+                Case 31 To 36
+                    myGraphics.DrawEllipse(myPen, x + (z * (drawnKeys(i) - 30)), y + (z * 5), 15, 15)
+                Case 37 To 42
+                    myGraphics.DrawEllipse(myPen, x + (z * (drawnKeys(i) - 36)), y + (z * 6), 15, 15)
+                Case 43 To 48
+                    myGraphics.DrawEllipse(myPen, x + (z * (drawnKeys(i) - 42)), y + (z * 7), 15, 15)
+                Case 49, 50
+                    myGraphics.DrawEllipse(myPen, x + (z * (drawnKeys(i) - 48)), y + (z * 8), 15, 15)
+                Case Else
+            End Select
+
+        Next
+
+    End Sub
+    Public Sub Drawstars(ByVal drawnStars() As Integer, ByVal imageCard As PictureBox) 'function to draw the stars, receives as a parameter the stars ordered in the array and the image box to be drawn
+        Dim myPen As Pen
+        myPen = New Pen(Drawing.Color.Green, 3)
+        Dim myGraphics As Graphics = imageCard.CreateGraphics
+        Dim i As Integer
+        Dim x As Integer = -24
+        Dim y As Integer = 275 'value varies according to the position of the image you want to draw
+        Dim z As Integer = 47
+
+        For i = 0 To drawnStars.Length - 1
+            Select Case drawnStars(i)
+                Case < 4
+                    myGraphics.DrawEllipse(myPen, x + (z * drawnStars(i)), y, 15, 15)
+                Case 4 To 6
+                    myGraphics.DrawEllipse(myPen, x + ((z + 3) * (drawnStars(i) - 3)), (y - 6) + (z * 1), 15, 15)
+                Case 7 To 9
+                    myGraphics.DrawEllipse(myPen, x + ((z + 3) * (drawnStars(i) - 6)), (y - 12) + (z * 2), 15, 15)
+                Case 10 To 12
+                    myGraphics.DrawEllipse(myPen, x + ((z + 3) * (drawnStars(i) - 9)), (y - 14) + (z * 3), 15, 15)
+                Case Else
+            End Select
+
+        Next
+
+    End Sub
+    Public Function GetRandom(ByVal Min As Integer, ByVal Max As Integer) As Integer
+        ' by making Generator static, we preserve the same instance '
+        ' (i.e., do not create new instances with the same seed over and over) '
+        ' between calls '
+        Static Generator As System.Random = New System.Random()
+        Return Generator.Next(Min, Max)
+    End Function
+
+    Public Function GenerateRandomNumber(ByVal randoms() As Integer, ByVal initial As Integer, ByVal final As Integer)
+        Dim unorderedKey As String
+        unorderedKey = ""
+        Dim number As Integer
+        Dim i As Integer
+        Dim equalNumber As Boolean
+
+        For i = 0 To randoms.Length - 1 'just go through the array size
+            equalNumber = False ' always starts false
+            number = GetRandom(initial, final) ' randomly generates a number between initial and final
+            For y As Integer = 0 To i
+                If (randoms(y) = number) Then 'checks if in position y is equal to the number generated
+                    equalNumber = True ' the "equalnumber" becomes true if the numbers has been found in the array
+                    Exit For
+                End If
+            Next
+            If (equalNumber) Then ' if the number exists in the array then the "i" returns  
+                i = i - 1 ' i-1 to be able to generate the number again
+            Else
+                randoms(i) = number 'if the number does not exist in then it is inserted
+                unorderedKey = unorderedKey & " - " & randoms(i)
+            End If
+
+        Next
+        Array.Sort(randoms)
+
+        Return randoms
+
+    End Function
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox1.Items.Clear() 'clear the listbox
+        ListBox2.Items.Clear()
+        PictureBox1.Image = Nothing 'clear the picturebox
+        PictureBox2.Image = Nothing
+        PictureBox3.Image = Nothing
+        PictureBox4.Image = Nothing
+        PictureBox5.Image = Nothing
+        ComboBox1.Text = Nothing ' clear the comboBox
+    End Sub
+End Class
